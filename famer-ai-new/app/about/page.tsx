f3dc41a5196687a596ca857596d6a5c17af571f9
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-07-08 15:20:39 +0100

    little change

---
+++ famer-ai-new/app/about/page.tsx
@@ -0,0 +1,13 @@
+import NavText from '@/components/NavText';
+import React from 'react'
+
+function About(){
+  return (
+    <>
+    <NavText/>
+    <div>this is the about page</div>
+    </>
+  )
+}
+
+export default About;
